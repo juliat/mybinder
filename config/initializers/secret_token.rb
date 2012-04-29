6352c02565bfb193873aba4016ad4f5755a9260f
Julia Teitelbaum <juliat@cmu.edu>
2012-04-29 16:56:45 -0400

    rails generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mybinder::Application.config.secret_token = 'e45f38f4f0d44576ec3695a3419e5a00b7810fff5391a0be1748b6dd1d06bdb73b604414a427e75aedf2a1f6781f14eee2e3d7d84420d9769905168a26a61520'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mybinder::Application.config.session_store :cookie_store, key: '_mybinder_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mybinder::Application.config.session_store :active_record_store
