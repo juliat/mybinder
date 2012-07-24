b57807f23fc2fb216552d38848cae78c89974bb7
Julia Teitelbaum <julialt@gmail.com>
2012-07-23 22:36:31 -0400

    removed accidentally crated rails app directory

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'eb847c1b8999e635ac1bf3a09bfef3082d08b962c979624d6af84b1600068b26ddb98d2c0ce52749635cd43e356b287b73361e92397f871d6227932ac13e3a25';
